856cae80ca53380aa6f56152f31bba86ddb5b82c
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-11-13 22:54:40 +0600

    Solved some problems

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import useAuth from '../../../hooks/useAuth';
 import MyOrder from '../MyOrder/MyOrder';
 
